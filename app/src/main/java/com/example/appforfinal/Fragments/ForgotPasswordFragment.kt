4f75e0e705ef766f8d570bc18e0464b311183299
giorgi-kavtiashvili <115482579+giorgi-kavtiashvili@users.noreply.github.com>
2023-01-14 13:21:41 +0400

    layoutebi davukavshire taviant fragmentebs

---
+++ app/src/main/java/com/example/appforfinal/Fragments/ForgotPasswordFragment.kt
@@ -0,0 +1,7 @@
+package com.example.appforfinal.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.appforfinal.R
+
+class ForgotPasswordFragment : Fragment(R.layout.fragment_forgot_password) {
+}
