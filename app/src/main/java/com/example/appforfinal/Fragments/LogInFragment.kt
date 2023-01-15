a1319aa6b1b5d1fa0f67d13c0a943e7315bb7281
SandroMatch <sandro.machavariani.1@bru.edu.ge>
2023-01-15 17:27:21 +0400

    gasworebuli fragmentebi(2clickze mushaobs marto)

--- app/src/main/java/com/example/appforfinal/Fragments/LogInFragment.kt
+++ app/src/main/java/com/example/appforfinal/Fragments/LogInFragment.kt
@@ -1,7 +1,11 @@
 package com.example.appforfinal.Fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.appforfinal.R
 
 class LogInFragment : Fragment(R.layout.fragment_login) {
+
 }
--- app/src/main/java/com/example/appforfinal/MainActivity.kt
+++ app/src/main/java/com/example/appforfinal/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.appforfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import androidx.fragment.app.Fragment
+import com.example.appforfinal.Fragments.HomeFragment
+import com.example.appforfinal.Fragments.ProfileFragment
+import com.example.appforfinal.Fragments.UploadImageFragment
+import com.example.appforfinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.profileFragment -> replaceFragment(ProfileFragment())
+                R.id.imageFragment -> replaceFragment(UploadImageFragment())
+
+                else ->{
+
+                }
+            }
+
+            true
+
+        }
+
+
     }
+    private fun replaceFragment(fragment : Fragment){
+
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
